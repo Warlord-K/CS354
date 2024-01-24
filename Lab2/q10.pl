16c41713b2238b565033c6081af3ea9f2136676e
Yatharth Gupta <95569637+Warlord-K@users.noreply.github.com>
2024-01-24 14:47:28 +0000

    Lab2 Final

--- Lab2/q10.pl
+++ Lab2/q10.pl
@@ -1,26 +1,25 @@
-lsort(InList,OutList) :-
-	add_key(InList,KList),
-	keysort(KList,SKList),
-	rem_key(SKList,OutList).
-
 add_key([],[]).
-add_key([X|Xs],[L-p(X)|Ys]) :-!, 
-	length(X,L), add_key(Xs,Ys).
+add_key([A|A1],[L-p(A)|B1]) :-!, length(A,L), add_key(A1,B1).
 
 rem_key([],[]).
-rem_key([_-p(X)|Xs],[X|Ys]) :- rem_key(Xs,Ys).
+rem_key([_-p(A)|A1],[A|B1]) :- rem_key(A1,B1).
 
-lfsort(InList,OutList) :-
-	add_key(InList,KList),
-	keysort(KList,SKList),
-	pack(SKList,PKList),
-	lsort(PKList,SPKList),
-	flatten(SPKList,FKList),
-	rem_key(FKList,OutList).
+lsort(In,Out) :-
+	add_key(In,K),
+	keysort(K,SK),
+	rem_key(SK,Out).
 
 pack([],[]).
-pack([L-X|Xs],[[L-X|Z]|Zs]) :- transf(L-X,Xs,Ys,Z), pack(Ys,Zs).
+pack([L-A|A1],[[L-A|C]|C1]) :- transf(L-A,A1,B1,C), pack(B1,C1).
 
 transf(_,[],[],[]).
-transf(L-_,[K-Y|Ys],[K-Y|Ys],[]) :- L \= K.
-transf(L-_,[L-X|Xs],Ys,[L-X|Zs]) :- transf(L-X,Xs,Ys,Zs).
+transf(L-_,[K-B|B1],[K-B|B1],[]) :- L \= K.
+transf(L-_,[L-A|A1],B1,[L-A|C1]) :- transf(L-A,A1,B1,C1).
+
+lfsort(In,Out) :-
+	add_key(In,K),
+	keysort(K,SK),
+	pack(SK,PK),
+	lsort(PK,SPK),
+	flatten(SPK,FK),
+	rem_key(FK,Out).
--- Lab2/q9.pl
+++ Lab2/q9.pl
@@ -1,11 +1,10 @@
-lsort(InList,OutList) :-
-	add_key(InList,KList),
-	keysort(KList,SKList),
-	rem_key(SKList,OutList).
-
 add_key([],[]).
-add_key([X|Xs],[L-p(X)|Ys]) :-!, 
-	length(X,L), add_key(Xs,Ys).
+add_key([A|A1],[L-p(A)|B1]) :-!, length(A,L), add_key(A1,B1).
 
 rem_key([],[]).
-rem_key([_-p(X)|Xs],[X|Ys]) :- rem_key(Xs,Ys).
+rem_key([_-p(A)|A1],[A|B1]) :- rem_key(A1,B1).
+
+lsort(In,Out) :-
+	add_key(In,K),
+	keysort(K,SK),
+	rem_key(SK,Out).
